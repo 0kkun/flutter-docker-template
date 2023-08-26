45442c6a582519ce92ec98a6c7b97adbb77c0a13
0kkun <shinji.okumoto.01@gmail.com>
2023-08-26 12:16:42 +0900

    json変換処理をサービスディレクトリへ移動

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -14,14 +14,11 @@ import 'package:workspace/screens/checkbox_screen.dart';
 import 'package:workspace/screens/progress_screen.dart';
 import 'package:workspace/screens/freezed_screen.dart';
 import 'package:workspace/screens/carousel_screen.dart';
-import 'dart:convert'; // これを書くとjsonDecodeが使える
 import 'package:flutter/services.dart'; // これを書くとStubが使える
-import 'package:workspace/models/vegetable/vegetable.dart';
-import 'package:workspace/models/pack/pack.dart'; 
-import 'package:workspace/models/recipe/recipe.dart';
 import 'package:flutter/foundation.dart';
 import 'package:workspace/screens/google_font_screen.dart';
 import 'package:workspace/screens/accordion_screen.dart';
+import 'package:workspace/services/json_service.dart';
 
 
 final logger = Logger();
@@ -367,7 +364,6 @@ void main() {
   });
 
   const app18 = MaterialApp(home: GoogleFontPage());
-
   // runApp(app18);
 
   // ******************************************
@@ -377,118 +373,4 @@ void main() {
   const app19 = MaterialApp(home: AccordionScreen());
 
   runApp(app19);
-}
-
-  // レベル1 (JSONを受け取る)
-void test1() async {
-  // Stub を使う準備
-  WidgetsFlutterBinding.ensureInitialized();
-  // JSON <--- Stub
-  final json = await rootBundle.loadString('jsons/level1.json');
-  // JsonMap <--- JSON
-  final map = jsonDecode(json);
-  // 野菜データ <--- JsonMap
-  final data = Vegetable.fromJson(map);
-  // データの中身を確認する
-  debugPrint('データの中身は $data');
-}
-
-// レベル1 (JSONを送る)
-void test2() async {
-  // 野菜データを作る
-  const data = Vegetable(
-    name: 'キャベツ',
-    color: 'みどり',
-    season: '春と冬',
-  );
-  // JsonMap <--- データ
-  final map = data.toJson();
-  // JSON <--- JsonMap
-  final json = jsonEncode(map);
-  // JSONの中身を確認する
-  debugPrint('JSONの中身は $json');
-}
-
-// レベル2 (JSONを受け取る)
-void test3() async {
-  // Stub を使う準備
-  WidgetsFlutterBinding.ensureInitialized();
-  // JSON <--- Stub
-  final json = await rootBundle.loadString('jsons/level2.json');
-  // JsonMap <--- JSON
-  final map = jsonDecode(json);
-  // 野菜パック <--- JsonMap
-  final data = Pack.fromJson(map);
-  // データの中身を確認する
-  debugPrint('データの中身は $data');
-}
-
-// レベル2 (JSONを送る)
-void test4() async {
-  // 適当な野菜
-  const content = Vegetable(
-    name: 'アボガド',
-    color: '濃いみどり',
-    season: '秋',
-  );
-  // 野菜パックのデータ
-  const data = Pack(
-    size: '中',
-    price: 800,
-    content: content,
-  );
-  // JsonMap <--- データ
-  final map = data.toJson();
-  // JSON <--- JsonMap
-  final json = jsonEncode(map);
-  // JSONの中身を確認する
-  debugPrint('JSONの中身は $json');
-}
-
-// レベル3 (JSONを受け取る)
-void test5() async {
-  // Stub を使う準備
-  WidgetsFlutterBinding.ensureInitialized();
-  // JSON <--- Stub
-  final json = await rootBundle.loadString('jsons/level3.json');
-  // JsonMap <--- JSON
-  final map = jsonDecode(json);
-  // レシピのデータ <--- JsonMap
-  final data = Recipe.fromJson(map);
-  // データの中身を確認する
-  debugPrint('データの中身は $data');
-}
-
-// レベル3 (JSONを送る)
-void test6() async {
-  // レシピに使う適当な野菜たち
-  const vegetables = [
-    Vegetable(
-      name: 'しょうが',
-      color: '黄色',
-      season: '秋',
-    ),
-    Vegetable(
-      name: 'かぶ',
-      color: '白',
-      season: '冬',
-    ),
-    Vegetable(
-      name: 'たけのこ',
-      color: '茶色',
-      season: '春',
-    ),
-  ];
-  // レシピのデータ
-  const data = Recipe(
-    title: '健康スープ',
-    calories: 150,
-    vegetables: vegetables,
-  );
-  // JsonMap <--- データ
-  final map = data.toJson();
-  // JSON <--- JsonMap
-  final json = jsonEncode(map);
-  // JSONの中身を確認する
-  debugPrint('JSONの中身は $json');
 }
